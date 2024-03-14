c5b5570fc099c1023479d94718a4b95f4327abe1
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2024-03-14 11:33:12 +0300

    create struct

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    re_path('auth/', include('djoser.urls.jwt')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
